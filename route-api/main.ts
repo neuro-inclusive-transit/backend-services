b6b9e810467163b5fdee36de1cc1344ed0ae3094
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-04-17 21:45:43 +0200

    add databases

--- route-api/main.ts
+++ route-api/main.ts
@@ -1,22 +1,30 @@
 import mqtt from "mqtt";
+import mariadb from "mariadb";
 
 const BROKER_HOST = Deno.env.get("BROKER_HOST") || "localhost";
 const BROKER_PORT = Deno.env.get("BROKER_PORT") || "1883";
 
-const client = mqtt.connect(`mqtt://${BROKER_HOST}:${BROKER_PORT}`);
+const DB_HOST = Deno.env.get("DB_HOST") || "localhost";
+const DB_PORT = Deno.env.get("DB_PORT")
+  ? parseInt(Deno.env.get("DB_PORT")!)
+  : 3306;
+const DB_USER = Deno.env.get("DB_USER") || "root";
+const DB_PASSWORD = Deno.env.get("DB_PASSWORD") || "root";
 
-client.on("connect", () => {
-  client.subscribe("presence", function (err) {
+const mqtt_client = mqtt.connect(`mqtt://${BROKER_HOST}:${BROKER_PORT}`);
+
+mqtt_client.on("connect", () => {
+  mqtt_client.subscribe("presence", function (err) {
     if (!err) {
-      client.publish("presence", "Hello mqtt");
+      mqtt_client.publish("presence", "Hello mqtt");
     }
   });
 });
 
-client.on("message", (_, message) => {
+mqtt_client.on("message", (_, message) => {
   console.log(message.toString());
 });
 
-client.on("error", (error) => {
+mqtt_client.on("error", (error) => {
   console.log(error);
 });
