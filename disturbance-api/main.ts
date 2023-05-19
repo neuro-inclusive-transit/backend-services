326ef44a6c862f71ea91335f1841eedf0f084a56
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-05-19 12:29:22 +0200

    add api-basic-setup

--- disturbance-api/main.ts
+++ disturbance-api/main.ts
@@ -1,7 +1,10 @@
 import mqtt from "mqtt";
+import { Application } from "oak/mod.ts";
 
 const BROKER_HOST = Deno.env.get("BROKER_HOST") || "localhost";
 const BROKER_PORT = Deno.env.get("BROKER_PORT") || "1883";
+
+const PORT = Deno.env.get("PORT") ? parseInt(Deno.env.get("PORT")!) : 3306;
 
 const client = mqtt.connect(`mqtt://${BROKER_HOST}:${BROKER_PORT}`);
 
@@ -20,3 +23,11 @@ client.on("message", (_, message) => {
 client.on("error", (error) => {
   console.log(error);
 });
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: PORT });
