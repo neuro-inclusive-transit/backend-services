395349b64a10b42f0c62ff8a55b9f3d1db19fb5f
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-05-28 23:04:23 +0200

    first try - script for fetching StationData

---
+++ challenge-api/getstationnr-script.ts
@@ -0,0 +1,20 @@
+const DB_API_KEY = "";
+const DB_CLIENT_ID = "";
+
+async function getStationData() {
+  const response = await fetch(
+    "https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations",
+    {
+      method: "GET",
+      headers: {
+        "DB-Api-Key": DB_API_KEY,
+        "DB-Client-Id": DB_CLIENT_ID,
+        "accept": "application/json",
+      },
+    },
+  );
+  const data = await response.json();
+  return data;
+}
+
+Deno.writeTextFile("./stationData.txt", JSON.stringify(await getStationData()));
