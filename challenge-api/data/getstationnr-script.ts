f7c6850830c8f953c579b7f68be5d1451d0ea153
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-05-29 21:29:11 +0200

    first try here-id

--- challenge-api/data/getstationnr-script.ts
+++ challenge-api/data/getstationnr-script.ts
@@ -20,6 +20,7 @@ async function loadEnvVariables(): Promise<Map<string, string>> {
 const envVariables = await loadEnvVariables();
 const DB_API_KEY = envVariables.get("DB_API_KEY")?.toString() || "noKey";
 const DB_CLIENT_ID = envVariables.get("DB_CLIENT_ID")?.toString() || "noKey";
+const HERE_API_KEY = envVariables.get("HERE_API_KEY")?.toString() || "noKey";
 
 type Station = {
   name: string;
@@ -29,6 +30,8 @@ type Station = {
 
 const stationDatafromDB = await getDBStationData();
 const stations: Station[] = [];
+
+getHereID("Hamburg Hbf");
 
 if (stationDatafromDB && stationDatafromDB.result) {
   stationDatafromDB.result.forEach((station: any) => {
@@ -66,3 +69,21 @@ async function getDBStationData() {
   const data = await response.json();
   return data;
 }
+
+/**
+
+Anmerkung: Das Abfragen der Here-ID ist bis auf weiteres pasuiert. Der AUfwand ist sehr groß, weil die API zusätzlich zum Namen auch Koordinaten benötigt
+
+async function getHereID(stationName: string) {
+  const response = await fetch(
+    `https://transit.hereapi.com/v8/stations?apiKey=${HERE_API_KEY}&r=1000000&in=50.94293888645432,6.959164616590985&name=${stationName}`,
+    {
+      method: "GET",
+    },
+  );
+  console.log(response.body);
+  // const data = await response.json();
+  // return data;
+}
+
+**/
