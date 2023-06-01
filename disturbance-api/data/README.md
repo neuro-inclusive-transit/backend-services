b38cfa6dd54bd60d34dd9e3d3ac0dc096de0b86c
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-06-01 17:29:25 +0200

    integratet querying DB-data into Disturbance-API

--- disturbance-api/data/README.md
+++
@@ -1,3 +0,0 @@
-Dieses Skript muss zu Beginn manuell ausgeführt werden. Es lädt die Stationen der StationData-API der DB und speichert die zugehörigen evaNr. der Stationen. In einem weiteren Schritt werden die zugehörigen IDs der Here-Services abgefragt und zu den Stationen gematcht.
-
-Ausführen des Skripts `deno run --allow-net --allow-write --allow-read getstationnr-script.ts`
--- disturbance-api/main.ts
+++ disturbance-api/main.ts
@@ -1,6 +1,10 @@
 import mqtt from "mqtt";
 import { Application } from "oak/mod.ts";
 
+const DB_API_KEY = Deno.env.get("DB_API_KEY") || "noKey";
+const DB_CLIENT_ID = Deno.env.get("DB_CLIENT_ID") || "noKey";
+const HERE_API_KEY = Deno.env.get("HERE_API_KEY") || "noKey";
+
 const BROKER_HOST = Deno.env.get("BROKER_HOST") || "localhost";
 const BROKER_PORT = Deno.env.get("BROKER_PORT") || "1883";
 
@@ -24,10 +28,58 @@ client.on("error", (error) => {
   console.log(error);
 });
 
-const app = new Application();
+type Station = {
+  name: string;
+  evaNr: string | null;
+};
 
-app.use(async (ctx) => {
-  ctx.response.body = "Hello World!";
-});
+async function getStationData() {
+  try {
+    console.log("Start getting data");
+    const stationDatafromDB = await getDBStationData();
+    console.log("Got Data from DB");
+    const stations = await minimizeData(stationDatafromDB);
+    console.log(stations);
+  } catch (error) {
+    console.error("Error retrieving station data:", error);
+  }
+}
 
-await app.listen({ port: PORT });
+async function minimizeData(stations: any): Promise<Station[]> {
+  const newstations: Station[] = [];
+
+  if (stations && stations.result) {
+    stations.result.forEach((station: any) => {
+      if (
+        station.mailingAddress.zipcode.startsWith("50") ||
+        station.mailingAddress.zipcode.startsWith("51")
+      ) {
+        const tmp: Station = {
+          name: station.name,
+          evaNr: station.evaNumbers[0]?.number || null,
+        };
+
+        newstations.push(tmp);
+      }
+    });
+  }
+  return newstations;
+}
+
+async function getDBStationData() {
+  const response = await fetch(
+    "https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations",
+    {
+      method: "GET",
+      headers: {
+        "DB-Api-Key": DB_API_KEY,
+        "DB-Client-Id": DB_CLIENT_ID,
+        accept: "application/json",
+      },
+    },
+  );
+  const data = await response.json();
+  return data;
+}
+
+getStationData();
