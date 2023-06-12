48460199310ed0d84aceb668410ad6a29feb7e26
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-06-12 22:35:22 +0200

    minimized parseTimetableData + publish delays + added intervall

--- disturbance-api/main.ts
+++ disturbance-api/main.ts
@@ -29,33 +29,9 @@ client.on("error", (error) => {
   console.log(error);
 });
 
-const messageTypes = {
-  h: "HIM (generiert von Hafas Information Manager)",
-  q: "Qualitätsänderung",
-  f: "Freitext",
-  d: "Verspätung",
-  i: "IBIS (generiert durch IRIS-AP)",
-  u: "IBIS (generiert durch IRIS-AP), noch nicht mit Zug verbunden",
-  r: "Ausfall",
-  c: "Verbindung/Connection",
-};
-const priorityTypes = {
-  1: "HIGH",
-  2: "MEDIUM",
-  3: "LOW",
-  4: "DONE",
-};
-const eventStatusTypes = {
-  p: "Ereignis geplant",
-  a: "Ereignis als neuer Halt hinzugefügt",
-  c: "Ereignis abgebrochen",
-};
-const stats = {
-  MeldungenPrioritaet: {},
-  AenderungsTyp: {},
-};
-
 const parseDate = (str, raw = false) => {
+  if (str == undefined) return null;
+
   const date = str.substr(0, 6);
   const time = str.substr(6, 4);
   const d = new Date();
@@ -73,6 +49,14 @@ const parseDate = (str, raw = false) => {
 type Station = {
   name: string;
   evaNr: string;
+};
+
+type Verspaetung = {
+  evaNr: string;
+  id: string;
+  linie: string;
+  arrivalTime: string;
+  newarrivalTime: string;
 };
 
 async function getStationData() {
@@ -93,8 +77,8 @@ function minimizeData(stations: any) {
   if (stations && stations.result) {
     stations.result.forEach((station: any) => {
       if (
-        station.mailingAddress.zipcode.startsWith("50127") ||
-        station.mailingAddress.zipcode.startsWith("50127")
+        station.mailingAddress.zipcode.startsWith("50") ||
+        station.mailingAddress.zipcode.startsWith("50")
       ) {
         const tmp: Station = {
           name: station.name,
@@ -139,61 +123,31 @@ async function getDBTimetableData(evaNr: string) {
 
 async function parseTimetableData(data: any) {
   const xml = create(await data.text()).end({ format: "object" });
-  stats.MeldungenAnzahl = xml.timetable.s.length;
 
-  const handleNachricht = (m, hideTime = false) => {
-    if (!Array.isArray(m)) m = [m];
-    return "Meldung: " + m.map((msg) => {
-      if (msg["@pr"]) {
-        stats.MeldungenPrioritaet[priorityTypes[msg["@pr"]]] =
-          (stats.MeldungenPrioritaet[priorityTypes[msg["@pr"]]] || 0) + 1;
-      }
-      return "ID: " + msg["@id"] +
-        (!hideTime ? ", veröffentlicht: " + parseDate(msg["@ts"]) : "") +
-        ", Typ: " + messageTypes[msg["@t"]] +
-        (msg["@from"] && !hideTime
-          ? ", gültig ab: " + parseDate(msg["@from"])
-          : "") +
-        (msg["@to"] && !hideTime ? ", bis: " + parseDate(msg["@to"]) : "") +
-        (msg["@cat"] ? ", Kategorie: " + msg["@cat"] : "") +
-        (msg["@pr"] ? ", Priorität: " + priorityTypes[msg["@pr"]] : "");
-    }).join("; ");
-  };
-
-  const meldung = xml.timetable.s.map((i) => {
+  xml.timetable.s.map((i) => {
     let temp = "ID: " + i["@id"];
-    const ardp = (j, typ) => {
-      return (j["@l"] ? "Linie: " + j["@l"] : "") +
-        (j["@ct"]
-          ? (j["@l"] ? ", " : "") + "geänderte " + typ + "szeit: " +
-            parseDate(j["@ct"])
-          : "") +
-        (j["@pt"] ? ", " + typ + "szeit geplant: " + parseDate(j["@pt"]) : "") +
-        (j["@cp"] ? ", geänderter Bahnsteig: " + j["@cp"] : "") +
-        (j["@pp"] ? ", Bahnsteig geplant: " + j["@pp"] : "") +
-        (j["@cpth"] ? ", geänderte Route: " + j["@cpth"] : "") +
-        (j["@ppth"] ? ", Route geplant: " + j["@ppth"] : "") +
-        (j["@cs"] ? ", Änderungsstatus: " + eventStatusTypes[j["@cs"]] : "") +
-        (j["@clt"] ? ", Halt entfernt um: " + parseDate(j["@clt"]) : "") +
-        (j.m ? ", " + handleNachricht(j.m) : "");
+
+    const ardp = (j) => {
+      const Verspaetung: Verspaetung = {
+        linie: j["@l"],
+        id: i["@id"],
+        evaNr: xml.timetable["@eva"],
+        arrivalTime: parseDate(j["@pt"]),
+        newarrivalTime: parseDate(j["@ct"]),
+      };
+      console.log(Verspaetung);
+      publishVerspaetung(Verspaetung);
     };
+    if (i.ar) temp += "; Ankunft: " + ardp(i.ar);
+  });
+}
 
-    if (i.ar) temp += "; Ankunft: " + ardp(i.ar, "Ankunft");
-    if (i.dp) temp += "; Abfahrt: " + ardp(i.dp, "Abfahrt");
-    if (i.tl) {
-      temp += "; Zug: " + i.tl["@c"] + " " + i.tl["@n"] + ", Filter: " +
-        i.tl["@f"] + ", Besitzer: " + i.tl["@o"] + ", Typ: " + i.tl["@t"];
-    }
-    if (i.m) temp += "; " + handleNachricht(i.m);
-    return temp;
-  }).join("\n");
-
-  console.log(
-    "Bahnhof: " + xml.timetable["@station"] + " (" + xml.timetable["@eva"] +
-      ")" +
-      (xml.timetable.m ? ", " + handleNachricht(xml.timetable.m, true) : ""),
+async function publishVerspaetung(data: Verspaetung) {
+  await client.publish(
+    data.evaNr + "/" + data.linie,
+    "newarrivalTime:" + data.newarrivalTime,
   );
-  console.log(meldung);
 }
 
 getStationData();
+setInterval(getStationData, 5 * 60 * 1000);
