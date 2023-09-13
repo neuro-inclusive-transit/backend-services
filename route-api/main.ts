0dc30344cd56b0a7fb3ad2073b635b281be03c6d
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-09-13 17:48:09 +0200

    put typedef in extra file

--- route-api/main.ts
+++ route-api/main.ts
@@ -2,6 +2,7 @@ import mqtt from "mqtt";
 import _mariadb from "mariadb";
 import { Application, Status } from "oak/mod.ts";
 import type { Context } from "oak/mod.ts";
+import { GetRouteOptions, HereApiRouteData } from "./routes.ts";
 
 const HERE_TRANSIT_API_KEY = Deno.env.get("HERE_TRANSIT_API") || "nokey";
 
@@ -37,118 +38,6 @@ mqtt_client.on("message", (_, message) => {
 mqtt_client.on("error", (error) => {
   console.log(error);
 });
-
-type LatLng = {
-  lat: number;
-  lng: number;
-};
-
-type Place = {
-  id?: string;
-  name?: string;
-  type: string;
-  evaNr?: string | null;
-  location: LatLng;
-};
-
-type TimeAndPlace = {
-  time: string;
-  place: Place;
-  delay?: number;
-};
-
-type GetRouteOptions = {
-  origin: string;
-  destination: string;
-  arrivalTime?: string;
-  departureTime?: string;
-  lang?: string;
-  units?: string;
-  changes?: number;
-  alternatives?: number;
-  modes?: string;
-  pedestrianSpeed?: number;
-  return?: string;
-};
-
-// @see https://developer.here.com/documentation/intermodal-routing/dev_guide/concepts/modes.html
-export type HereApiTransportMode =
-  | "highSpeedTrain"
-  | "intercityTrain"
-  | "interRegionalTrain"
-  | "regionalTrain"
-  | "cityTrain"
-  | "bus"
-  | "ferry"
-  | "subway"
-  | "lightRail"
-  | "privateBus"
-  | "inclined"
-  | "aerial"
-  | "busRapid"
-  | "monorail"
-  | "flight"
-  | "walk"
-  | "car"
-  | "bicycle"
-  | "pedestrian"
-  | string;
-
-type HereApiRouteData = {
-  routes: Array<{
-    id: string;
-    sections: Array<{
-      id: string;
-      type: string;
-      departure: TimeAndPlace;
-      arrival: TimeAndPlace;
-      summary?: {
-        duration: number;
-        length: number;
-      };
-      actions?: Array<{
-        action: string;
-        duration: number;
-        instruction: string;
-        direction?: string;
-        severity?: string;
-        offset?: number;
-        exit?: number;
-      }>;
-      polyline?: string;
-      spans?: Array<{
-        offset: number;
-        names: Array<{
-          value: string;
-          language: string;
-        }>;
-      }>;
-      transport: {
-        mode: HereApiTransportMode;
-        name?: string;
-        category?: string;
-        color?: string;
-        textColor?: string;
-        headsign?: string;
-        shortName?: string;
-      };
-      intermediateStops?: Array<{
-        departure: TimeAndPlace;
-        duration?: number;
-      }>;
-      agency?: {
-        id: string;
-        name: string;
-        website: string;
-      };
-    }>;
-    agency?: {
-      id: string;
-      name: string;
-      website: string;
-    };
-  }>;
-};
 
 const app = new Application();
 
