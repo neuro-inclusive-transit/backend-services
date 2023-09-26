24d3d56cf29d419b5104d3b8034367deab0623cc
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-09-26 23:41:50 +0200

    update import

--- route-api/test.ts
+++ route-api/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "assert/mod.ts";
+import { assertEquals } from "std/assert/mod.ts";
 
 import { generateDisturbanceApiURL } from "./aggregateData.ts";
 import { generateHereApiURL } from "./getHereApiData.ts";
